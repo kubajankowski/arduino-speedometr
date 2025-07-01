921868cb0f1f0200c4a6a1e0a9b477765a4e46f5
Jakub Jankowski <33807244+kubajankowski@users.noreply.github.com>
2025-07-01 11:13:53 +0200

    Add code.ino

---
+++ code.ino
@@ -0,0 +1,28 @@
+const int signalPin = 2;           
+volatile unsigned int pulses = 0;
+unsigned long previousTime = 0;
+float wheelCircumference = 2.1;
+int pulsesPerRevolution = 1;       
+
+void setup() {
+  pinMode(signalPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(signalPin), countPulse, RISING);
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long now = millis();
+  if (now - previousTime >= 1000) { // Every second
+    float revolutions = (float)pulses / pulsesPerRevolution;
+    float speed = (revolutions * wheelCircumference) * 3.6; // m/s to km/h
+    Serial.print("Speed km/h: ");
+    Serial.println(speed);
+
+    pulses = 0;
+    previousTime = now;
+  }
+}
+
+void countPulse() {
+  pulses++;
+}
